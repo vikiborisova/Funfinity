9c129f7c7ef892e6cda73c3fc556b92f79239bda
vikiborisova <novikovaviki2005@gmail.com>
2024-04-03 18:40:08 +0300

    home page responsive and link to games

--- home_page/index.js
+++ home_page/index.js
@@ -3,4 +3,3 @@ const checkboxTheme = document.getElementById("themeCheckbox");
 checkboxTheme.addEventListener("change", () => {
     document.body.classList.toggle("dark", checkboxTheme.checked());
 });
-
